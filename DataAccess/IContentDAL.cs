79c5d1897cd8c5e483b5d2ff128a5e05d03cca4a
Shrestha <mangal.shrestha@hireright.com>
2018-12-18 18:55:55 -0600

    Updated code for change request and refactored.

---
+++ DataAccess/IContentDAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleWebServer.DataAccess
+{
+    public interface IContentDAL
+    {
+        string GetContent(string resourceName);
+    }
+}
---
+++ Services/ContentService.cs
@@ -0,0 +1,24 @@
+﻿using ExampleWebServer.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleWebServer.Services
+{
+    public class ContentService:IContentService
+    {
+        IContentDAL _contentDAL;
+
+        public ContentService()
+        {
+            _contentDAL = new ContentDAL();
+        }
+
+        public string GetContent(string resourceName)
+        {
+            return _contentDAL.GetContent(resourceName);
+        }
+    }
+}
