2dfa8bc315a0f7a3e42f3baf853edf073b683d1b
Shrestha <mangal.shrestha@hireright.com>
2018-12-18 18:54:36 -0600

    Updated code for change request and refactored.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using ExampleWebServer.Services;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ExampleWebServer
@@ -16,18 +19,6 @@ namespace ExampleWebServer
             TcpListener server = null;
             try
             {
-                string connetionString = "Data Source=54.213.195.209;Initial Catalog=Example;User ID=example;Password=example";
-                SqlConnection cnn = new SqlConnection(connetionString);
-                try
-                {
-                    cnn.Open();                    
-                    cnn.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Can not open data connection");
-                }
-
                 Int32 port = 8081;
                 IPAddress localAddr = IPAddress.Parse("127.0.0.1");
                 server = new TcpListener(localAddr, port);
@@ -44,33 +35,26 @@ namespace ExampleWebServer
 
                     data = null;
                     NetworkStream stream = client.GetStream();
+                    
+                    data = GetRequest(stream);
+                    Console.WriteLine("{0}", data);
+                    RequestLoggerService.Log(data);
+                    var serviceResponse = new ServiceResponse(GetResourceName(data));
+                   
 
-                    int i;
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                    {
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.Write("{0}", data);
-                    }
-
-                    String body = @"<html><body>Hello world</body></html>";
-                    String response =
-@"HTTP/1.1 200 OK
-Server: Example
-Accept-Ranges: bytes
-Content-Length: " + body.Length.ToString() + @"
-Content-Type: text/html
-
-" + body;
-
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(response);
-                    stream.Write(msg, 0, msg.Length);
-                    Console.WriteLine("Sent: {0}", response);
+                    
+                    stream.Write(serviceResponse.ResponseMessage, 0, serviceResponse.ResponseMessage.Length);
+                    Console.WriteLine("Sent: {0}", serviceResponse.ResponseString);
                     client.Close();
                 }
             }
             catch (SocketException e)
             {
                 Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e);
             }
             finally
             {                 
@@ -79,6 +63,32 @@ Content-Type: text/html
 
             Console.WriteLine("\nHit enter to continue...");
             Console.Read();
+        }
+
+        private static string GetRequest(NetworkStream stream)
+        {
+            byte[] data = new byte[1024];
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                do
+                {
+                   stream.Read(data, 0, data.Length);
+                   memoryStream.Write(data, 0, data.Length);
+                } while (stream.DataAvailable);
+
+                return Encoding.ASCII.GetString(memoryStream.ToArray(), 0, (int)memoryStream.Length);
+            }
+        }
+
+        private static string GetResourceName(string data)
+        {
+            var requestData = data.Split('\r');
+            if(requestData.Length<2)
+            {
+                return "/";
+            }
+            var resourceName = requestData[0].Split(' ')[1];
+            return resourceName;
         }
     }
 }
