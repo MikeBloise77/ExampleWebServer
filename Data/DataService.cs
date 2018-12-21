31d1bfee9808af96f31fb20f88235f4d2b2cd383
Jorge Gabriel Altieri <altierijorge@gmail.com>
2018-12-21 02:18:15 -0500

    Server refactor.

---
+++ Data/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class DataService : IDataService
+{
+    public void Connect()
+    {
+        string connectionString = "Data Source=54.213.195.209;Initial Catalog=Example;User ID=example;Password=example";
+        SqlConnection cnn = new SqlConnection(connectionString);
+        try
+        {
+            cnn.Open();
+            cnn.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Can not open data connection");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Attributes;
+
+class Startup
+{
+    private IDataService _dataService;
+    private IListener _listener;
+
+    public Startup(IDataService dataService, IListener listener)
+    {
+        _dataService = dataService;
+        _listener = listener;
+    }
+
+    public void Init()
+    {
+        _dataService.Connect();
+        _listener.Listen();
+    }
+}
